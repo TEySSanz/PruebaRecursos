b217f3e0ee340cf41f533fbf393e9114b9f29e7a
David Sanz <dsanz@teys.eu>
2020-08-20 12:15:48 +0200

    Curso Android. Creación de Activity I . Vídeo 30

---
+++ app/src/main/java/eu/teys/dsanz/pruebarecursos/datos_adicionales_Class.java
@@ -0,0 +1,12 @@
+package eu.teys.dsanz.pruebarecursos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class datos_adicionales_Class extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datos_adicionales);
+    }
+}
