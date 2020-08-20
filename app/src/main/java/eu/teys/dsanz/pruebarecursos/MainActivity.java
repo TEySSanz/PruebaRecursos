a3bfb534f53e82a70f4113de3754bce367e3efdb
David Sanz <dsanz@teys.eu>
2020-08-20 12:14:12 +0200

    Curso Android. Creación de Activity I . Vídeo 30

--- app/src/main/java/eu/teys/dsanz/pruebarecursos/MainActivity.java
+++ app/src/main/java/eu/teys/dsanz/pruebarecursos/MainActivity.java
@@ -2,7 +2,9 @@ package eu.teys.dsanz.pruebarecursos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ejecutar_datos_adicionales_Class (View view){
+        Intent i = new Intent(this, datos_adicionales_Class.class);
+        startActivity(i);
     }
 }
